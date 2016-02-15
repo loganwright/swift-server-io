f1ee87aa48b41d0c3688370e923644fa6bf8d5e5
Logan Wright <logan.william.wright@gmail.com>
2016-02-14 23:28:59 -0500

    adding hello name url parameter

--- Sources/main.swift
+++ Sources/main.swift
@@ -15,5 +15,10 @@ Route.get("hello") { _ in
     return ["Hello" : "World"]
 }
 
+Route.get("hello/:name") { request in
+    let name = request.parameters["name"] ?? "World"
+    return ["Hello" : name]
+}
+
 let server = Server()
 server.run(port: 8080)
