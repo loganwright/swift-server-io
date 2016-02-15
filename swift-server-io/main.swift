55cc71055ccecc3848b3f3a1c230bf69f812f8ed
Logan Wright <logan.william.wright@gmail.com>
2016-02-14 22:39:32 -0500

    Initial Commit

---
+++ swift-server-io/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-server-io
+//
+//  Created by Logan Wright on 2/14/16.
+//  Copyright © 2016 LoganWright. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
